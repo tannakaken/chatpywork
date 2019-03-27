9f484ba03360fa084ff0dbda01bb64a133197994
tanakakensaku <tannakaken@gmail.com>
2019-03-28 00:02:28 +0900

    setup.pyを書いた

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='pychat',
+    version='1.0.0',
+    descrption='python wrapper for ChatWork API v2',
+    long_description=long_description,
+    url='https://github.com/tannakaken/pychat',
+    author='tannakaken',
+    author_email='tannakaken@gmail.com',
+    license='MIT',
+    install_requires=['requests'],
+    keywords='chatwork',
+    packages=find_packages(),
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7'
+    ],
+)
